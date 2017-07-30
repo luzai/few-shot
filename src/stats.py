41471f541b9c9cdc41dbc8d62bfe7e44bd626360
Wang Xinglu <907682447@qq.com>
2017-07-30 20:01:47 +0800

    Add stat: diff & fluctuation

--- src/stats.py
+++ src/stats.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Stat(object):
     def __init__(self, stat='all'):
         if stat == 'all':
@@ -36,8 +37,77 @@ class Stat(object):
     def pos_neg_rat(self, tensor):
         return float(tensor[tensor > 0].shape[0]) / float(tensor[tensor < 0].shape[0])
 
-    def calc_all(self,tensor,name):
-        res={}
-        for key,val in self.stat.iteritems():
-            res[name+'/'+key]=val(self,tensor)
-        return  res
+    def calc_all(self, tensor, name):
+        res = {}
+        for key, val in self.stat.iteritems():
+            res[name + '/' + key] = val(self, tensor)
+        return res
+
+    def mag_mean(self, tensor):
+        return np.abs(tensor).mean()
+
+
+class Diff(object):
+    def __init__(self):
+        self.l_iter = {}
+        self.l_tensor = {}
+
+    def diff(self, tensor, iter, name):
+        # mean
+        # todo iter 12 continue
+        res = {}
+        if iter == self.l_iter[name]:
+            res[name + '/' + 'diff'] = (tensor - self.l_tensor[name]).abs().mean()
+        self.l_iter[name] = iter
+        self.l_tensor[name] = tensor
+        return res
+
+
+class OnlineStd(object):
+    def __init__(self):
+        self.l_std = {}
+
+    def online_std(self, tensor, iter, name):
+        # todo sample per 100 make sure
+        if name not in self.l_std:
+            self.l_std[name] = _OnlineStd()
+        assert iter % 100 == 0, 'todo'
+        self.l_std[name].include(tensor)
+        return {name + '/' + 'online_std': self.l_std[name].std}
+
+
+class _OnlineStd(object):
+    """
+    Welford's algorithm computes the sample variance incrementally.
+    """
+
+    def __init__(self, iterable=None, ddof=1):
+        self.ddof, self.n, = ddof, 0
+
+        if iterable is not None:
+            for datum in iterable:
+                self.include(datum)
+
+    def include(self, datum):
+        if self.n == 0:
+            self.n += 1
+            self.mean, self.M2 = np.zeros_like(datum, dtype=np.float)
+        else:
+            self.n += 1
+            self.delta = datum - self.mean
+            self.mean += self.delta / self.n
+            self.M2 += self.delta * (datum - self.mean)
+
+    @property
+    def variance(self):
+        if self.n < 2:
+            return float('nan')
+        else:
+            return self.M2 / (self.n - self.ddof)
+
+    @property
+    def std(self):
+        if self.n < 2:
+            return float('nan')
+        else:
+            return np.sqrt(self.variance)
